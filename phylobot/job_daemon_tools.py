9e9fb75b606d8124e8601034e2c769d23dc60a9c
vhsvhs <victorhansonsmith@gmail.com>
2015-03-31 17:32:38 -0700

    Started building methods for cleaning orphaned instances.

--- phylobot/job_daemon_tools.py
+++ phylobot/job_daemon_tools.py
@@ -342,7 +342,7 @@ def cleanup_orphaned_instances(dbconn):
     conn = boto.ec2.connect_to_region(ZONE)
     reservation = conn.get_all_instances()[0]
     for ii in reservation.instances:
-        print ii
+        print ii.instance_type, ii.instance_type, ii.state, ii.ip_address
 
 def run_tests():
     dbconn = None
