bde6bdc9ae85b390eb363d23040b667411aa6c89
vhsvhs <victorhansonsmith@gmail.com>
2015-11-22 15:39:26 -0800

    Adding a new migration script

--- aws_setup/migrate.sh
+++ aws_setup/migrate.sh
@@ -5,14 +5,14 @@
 
 # Copy the static data, including uploaded sequences.
 cd
-scp -i ~/.ssh/phylobot-ec2-key.pem -r ubuntu@phylobot:/home/ubuntu/phylobot-django/phylobot/static/* /home/ubuntu/phylobot-django/phylobot/static/
+scp -i ~/.ssh/phylobot-ec2-key.pem -r ubuntu@www.phylobot.com:/home/ubuntu/phylobot-django/phylobot/static/* /home/ubuntu/phylobot-django/phylobot/static/
 sudo chown www-data -r /home/ubuntu/phylobot-django/phylobot/static
 sudo chgrp www-data -r /home/ubuntu/phylobot-django/phylobot/static
 
 # Copy the main user and data DB. It's owned by www-data
-scp -i ~/.ssh/phylobot-ec2-key.pem ubuntu@phylobot:/home/ubuntu/phylobot-django/phylobot/db.sqlite3 /home/ubuntu/phylobot-django/phylobot/
+scp -i ~/.ssh/phylobot-ec2-key.pem ubuntu@www.phylobot.com:/home/ubuntu/phylobot-django/phylobot/db.sqlite3 /home/ubuntu/phylobot-django/phylobot/
 sudo chown www-data -r /home/ubuntu/phylobot-django/phylobot/db.sqlite3
 sudo chgrp www-data -r /home/ubuntu/phylobot-django/phylobot/db.sqlite3
 
 # Copy the Job daemon DB. It remains owned by root.
-scp -i ~/.ssh/phylobot-ec2-key.pem ubuntu@phylobot:/home/ubuntu/phylobot-django/phylobot/job_daemon.db /home/ubuntu/phylobot-django/phylobot/
+scp -i ~/.ssh/phylobot-ec2-key.pem ubuntu@www.phylobot.com:/home/ubuntu/phylobot-django/phylobot/job_daemon.db /home/ubuntu/phylobot-django/phylobot/
