5cb7e72c25615b715f06a9fec3a865444ed28696
vhsvhs <victorhansonsmith@gmail.com>
2015-11-28 14:21:23 -0800

    Adding custom 500 and 404 pages

--- aws_setup/migrate.sh
+++ aws_setup/migrate.sh
@@ -6,32 +6,22 @@
 
 # Copy the static data, including uploaded sequences.
 cd
-#sudo chown www-data -R /home/ubuntu/phylobot-django/phylobot
-#sudo chgrp www-data -R /home/ubuntu/phylobot-django/phylobot
 
 scp -i ~/.ssh/phylobot-ec2-key.pem -r ubuntu@www.phylobot.com:/home/ubuntu/phylobot-django/phylobot/static/* /home/ubuntu/phylobot-django/phylobot/static/
-#sudo chown www-data -R /home/ubuntu/phylobot-django/phylobot/static
-#sudo chgrp www-data -R /home/ubuntu/phylobot-django/phylobot/static
 
 # Copy the main user and data DB. It's owned by www-data
 scp -i ~/.ssh/phylobot-ec2-key.pem ubuntu@www.phylobot.com:/home/ubuntu/phylobot-django/phylobot/db.sqlite3 /home/ubuntu/phylobot-django/phylobot/
-#sudo chown www-data /home/ubuntu/phylobot-django/phylobot/db.sqlite3
-#sudo chgrp www-data /home/ubuntu/phylobot-django/phylobot/db.sqlite3
 
 # Copy the Job daemon DB. It remains owned by root.
 scp -i ~/.ssh/phylobot-ec2-key.pem ubuntu@www.phylobot.com:/home/ubuntu/phylobot-django/phylobot/job_daemon.db /home/ubuntu/phylobot-django/phylobot/
 
 # Copy the BOTO configuration
 sudo scp -i ~/.ssh/phylobot-ec2-key.pem -r ubuntu@www.phylobot.com:/etc/boto.cfg /etc/boto.cfg
-#sudo chown ubuntu /etc/boto.cfg
-#sudo chgrp ubuntu /etc/boto.cfg
 
 
-
-
-
-
-
+#
+# (depricated -- from Apache build)
+#
 ## Copy the static data, including uploaded sequences.
 #cd
 #sudo chown www-data -R /home/ubuntu/phylobot-django/phylobot
@@ -53,3 +43,4 @@ sudo scp -i ~/.ssh/phylobot-ec2-key.pem -r ubuntu@www.phylobot.com:/etc/boto.cfg
 #sudo scp -i ~/.ssh/phylobot-ec2-key.pem -r ubuntu@www.phylobot.com:/etc/boto.cfg /etc/boto.cfg
 #sudo chown ubuntu /etc/boto.cfg
 #sudo chgrp ubuntu /etc/boto.cfg
+#
--- phylobot/phylobot/views.py
+++ phylobot/phylobot/views.py
@@ -35,3 +35,16 @@ def examples(request):
 
 def overview(request):
     return render_to_response('learnmore.html')
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
