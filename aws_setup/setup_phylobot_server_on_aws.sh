aa0f2266f127b479a7920e85b3c84ec6a5c96e61
vhsvhs <victorhansonsmith@gmail.com>
2015-11-28 17:16:42 -0800

    Adding init.d script

--- aws_setup/setup_phylobot_server_on_aws.sh
+++ aws_setup/setup_phylobot_server_on_aws.sh
@@ -37,11 +37,11 @@ sudo apt-get -y install python-pip
 #
 sudo apt-get -y install postgresql postgresql-contrib libpq-dev
 sudo -u postgres psql -c "CREATE DATABASE phylobotdb"
-sudo -u postgres psql --dbname=csbotdb -c "CREATE USER django WITH PASSWORD 'phylobotpass'"
-sudo -u postgres psql --dbname=csbotdb -c "ALTER ROLE django SET timezone TO 'UTC-8'"
-sudo -u postgres psql --dbname=csbotdb -c "ALTER USER django CREATEDB"
-sudo -u postgres psql --dbname=csbotdb -c "GRANT ALL PRIVILEGES ON DATABASE phylobotdb TO django"
-sudo -u postgres psql --dbname=csbotdb -c "ALTER USER CREATEDB"
+sudo -u postgres psql --dbname=phylobotdb -c "CREATE USER django WITH PASSWORD 'phylobotpass'"
+sudo -u postgres psql --dbname=phylobotdb -c "ALTER ROLE django SET timezone TO 'UTC-8'"
+sudo -u postgres psql --dbname=phylobotdb -c "ALTER USER django CREATEDB"
+sudo -u postgres psql --dbname=phylobotdb -c "GRANT ALL PRIVILEGES ON DATABASE phylobotdb TO django"
+sudo -u postgres psql --dbname=phylobotdb -c "ALTER USER CREATEDB"
 
 #
 # Install Python packages
