3aed3c696f295bedf0191609595fedbb6f558d2d
vhsvhs <victorhansonsmith@gmail.com>
2015-11-29 00:41:30 -0800

    Moving from init.d to upstart

--- aws_setup/build_upstart_scripts.py
+++ aws_setup/build_upstart_scripts.py
@@ -8,11 +8,9 @@ export_lines = []
 fin = open(bashscriptpath, "r")
 
 for line in fin.xreadlines():
-    if False == line.startswith("export"):
-        fout.write(line)
-        continue
-    newline = re.sub("export ", "env ", line)
-    export_lines.append( newline )
+    if line.startswith("export"):
+        newline = re.sub("export ", "env ", line)
+        export_lines.append( newline )
 fin.close()
 
 fin = open(configpath_erg, "r")
