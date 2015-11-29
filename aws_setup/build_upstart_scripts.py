0fbf1b0f2cb9aaea22a83427b8a4e84ceb9ed012
vhsvhs <victorhansonsmith@gmail.com>
2015-11-29 00:40:18 -0800

    Moving from init.d to upstart

---
+++ aws_setup/build_upstart_scripts.py
@@ -0,0 +1,27 @@
+import os, re, sys
+
+configpath_erg = sys.argv[1]
+configpath_final = sys.argv[2]
+bashscriptpath = "/home/ubuntu/.phylobot"
+
+export_lines = []
+fin = open(bashscriptpath, "r")
+
+for line in fin.xreadlines():
+    if False == line.startswith("export"):
+        fout.write(line)
+        continue
+    newline = re.sub("export ", "env ", line)
+    export_lines.append( newline )
+fin.close()
+
+fin = open(configpath_erg, "r")
+fout = open(outpath, "w")
+for line in fin.xreadlines():
+    if False == line.startswith("#ENV"):
+        fout.write(line)
+        continue
+    for l in export_lines:
+        fout.write(l)
+fout.close()
+fin.close()
