56cf7bd1adb7c4584c3ccbcca26dca794a08f830
vhsvhs <victorhansonsmith@gmail.com>
2015-11-30 16:59:30 -0800

    Adding celery support, replacing the old job_daemon

---
+++ phylobot/portal/tasks.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from phylobot.celery import shared_task
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
