dc50014f6341c3c8e9169cc35b99a958700ef456
vhsvhs <victorhansonsmith@gmail.com>
2015-11-30 17:03:35 -0800

    Adding celery support, replacing the old job_daemon

--- phylobot/phylobot/settings.py
+++ phylobot/phylobot/settings.py
@@ -174,5 +174,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+#
+# Celery Stuff
+#
+BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_RESULT_BACKEND = 'amqp://localhost:5672'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
 
