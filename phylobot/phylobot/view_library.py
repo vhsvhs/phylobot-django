b746a52b09b6e410e8e941da09f11512e246de43
vhsvhs <victorhansonsmith@gmail.com>
2015-12-30 15:15:08 -0800

    .

--- phylobot/phylobot/view_library.py
+++ phylobot/phylobot/view_library.py
@@ -868,6 +868,7 @@ def get_ancestralstates_helper(con, ancid):
     sql = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='" + tablename + "'"
     cur.execute(sql)
     x = cur.fetchone()
+    print "871:", x
     if x[0] == 0:
         use_legacy == True
         tablename = "AncestralStates"
