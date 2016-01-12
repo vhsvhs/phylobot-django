6b011ea368ebd4d795be72632ea1c461079446c5
vhsvhs <victorhansonsmith@gmail.com>
2016-01-11 22:07:03 -0800

    Optimizing the get_ml_vectors SQL query

--- phylobot/phylobot/view_library.py
+++ phylobot/phylobot/view_library.py
@@ -1599,7 +1599,7 @@ def view_ancestors_aligned(request, alib, con, render_csv=False):
         #ancvectors.append( (ancname, ancid_vector[ancid]) )
         this_vector = ""
         for ii in ancid_vector[ancid]:
-            this_vector += "<td>" + ii[0] + "(%.3f"%ii[1] + ")</td>"
+            this_vector += "<td>" + ii[0] + "(" + ii[1].__str__() + ")</td>"
         ancvectors.append( (ancname,this_vector) )
     
     context["msanames"] = get_alignmentnames(con)
