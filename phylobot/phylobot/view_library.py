d1780b2c711957dfaf691f24502593ac33e9a158
vhsvhs <victorhansonsmith@gmail.com>
2016-01-13 13:04:11 -0800

    Adding the ancestral search tool

--- phylobot/phylobot/view_library.py
+++ phylobot/phylobot/view_library.py
@@ -1803,6 +1803,12 @@ def view_ancestors_search(request, alib, con):
                 
                 t = Tree()
                 t.read_from_string(cladonewick, "newick")
+                
+                print "1807:", t.__str__()
+                
+                for taxonname in taxon_labels:
+                    if taxonname not in t.taxon_namespace:
+                        print "1809:", taxonname, "not in the tree"
                 #print t
                 #print taxon_labels
                 mrca = t.mrca( taxon_labels=taxon_labels )
